b5dec3bc832ef44a4d9cb2d2681a1386e2182e33
Idris Yusuf <yusid03@gmail.com>
2023-09-12 22:52:07 +0100

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,15 @@
 import { handleApiError } from "../utils/errorHandler";
+import axios from "axios";
 
-const API_KEY = "1aef3fe5661608b3f53ac9295e701e95";
+const API_KEY = '1aef3fe5661608b3f53ac9295e701e95';
 
 export const fetchTopMovies = async () => {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
     );
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch top movies: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.results.slice(0, 10);
+    return response.data.results.slice(0, 10);
   } catch (error) {
     const errorMessage = handleApiError(error);
     throw new Error(errorMessage);
@@ -22,20 +18,11 @@ export const fetchTopMovies = async () => {
 
 export const fetchMovieDetails = async (movieId) => {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos,images`
     );
 
-    if (!response.ok) {
-      if (response.status === 401) {
-        throw new Error(`Unauthorized: ${response.status}`);
-      } else {
-        throw new Error(`Failed to fetch movie details: ${response.status}`);
-      }
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
     const errorMessage = handleApiError(error);
     throw new Error(errorMessage);
@@ -44,16 +31,11 @@ export const fetchMovieDetails = async (movieId) => {
 
 export const searchMovies = async (query) => {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
     );
 
-    if (!response.ok) {
-      throw new Error(`Failed to search for movies: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.results;
+    return response.data.results;
   } catch (error) {
     const errorMessage = handleApiError(error);
     throw new Error(errorMessage);
