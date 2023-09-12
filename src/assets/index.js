71e442ec9bcb81a83b582d2f8e877c9bd0033e06
Idris Yusuf <yusid03@gmail.com>
2023-09-12 21:21:56 +0100

    assets index

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import imdb from './imdb.png'
+import Logo from './Logo.png'
+import menu from './red-menu.png'
+import poster from './poster.png'
+import searchicon from './search.png'
+import rt from './rt.png'
+import logosidebar from './Logo_2.png'
+
+
+export { imdb, Logo, menu, poster, searchicon, rt, logosidebar }
