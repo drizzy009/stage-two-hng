16ceb1181bc575e774bc23c15d7081a050fde2d0
Idris Yusuf <yusid03@gmail.com>
2023-09-13 14:55:48 +0100

    asset update

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,7 @@ import poster from './poster.png'
 import searchicon from './search.png'
 import rt from './rt.png'
 import logosidebar from './Logo_2.png'
+import leftposter from './leftposter.webp'
 
 
-export { imdb, Logo, menu, poster, searchicon, rt, logosidebar }
+export { imdb, Logo, menu, poster, searchicon, rt, logosidebar, leftposter }
