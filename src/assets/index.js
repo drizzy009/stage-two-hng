010ee858dfdaf3195d1c97a602d05be91a8ff722
Idris Yusuf <yusid03@gmail.com>
2023-09-14 11:10:56 +0100

    likeToggle

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,8 @@ import searchicon from './search.png'
 import rt from './rt.png'
 import logosidebar from './Logo_2.png'
 import leftposter from './leftposter.webp'
+import unlike from './fave.png'
+import like from './red-fave.png'
 
 
-export { imdb, Logo, menu, poster, searchicon, rt, logosidebar, leftposter }
+export { imdb, Logo, menu, poster, searchicon, rt, logosidebar, leftposter, like, unlike }
--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
-import { imdb, rt } from "../../assets";
+import { imdb, rt, like, unlike } from "../../assets";
 
 const Card = ({ movie }) => {
   const toUTC = (dateString) => {
@@ -13,6 +14,21 @@ const Card = ({ movie }) => {
       timeZone: "UTC",
     };
     return date.toLocaleDateString("en-US", options);
+  };
+
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    const storedLiked = localStorage.getItem(`liked_${movie.id}`);
+    if (storedLiked) {
+      setLiked(JSON.parse(storedLiked));
+    }
+  }, [movie.id]);
+
+  const toggleLike = () => {
+    const newLiked = !liked;
+    setLiked(newLiked);
+    localStorage.setItem(`liked_${movie.id}`, JSON.stringify(newLiked));
   };
 
   return (
@@ -29,17 +45,30 @@ const Card = ({ movie }) => {
           {toUTC(movie.release_date)}
         </span>
         <span data-testid="movie-title">{movie.title}</span>
-        </Link>
-        <div className={styles.rating}>
-          <span data-testid="movie-ratings">
-            <img src={imdb} alt={imdb} />
-            {movie.vote_average}/100
-          </span>
-          <span data-testid="movie-ratings">
-            <img src={rt} alt={rt} />
-            {movie.vote_average * 10}%
-          </span>
-        </div>
+      </Link>
+      <div className={styles.likeToggle}>
+        <button
+          className={liked ? styles.likedButton : styles.likeButton}
+          onClick={toggleLike}
+        >
+          {liked ? (
+            <img src={like} alt="Unlike" />
+          ) : (
+            <img src={unlike} alt="Like" />
+          )}
+        </button>
+      </div>
+      <div className={styles.rating}>
+        <span data-testid="movie-ratings">
+          <img src={imdb} alt={imdb} />
+          {movie.vote_average}/100
+        </span>
+        <span data-testid="movie-ratings">
+          <img src={rt} alt={rt} />
+          {movie.vote_average * 10}%
+        </span>
+      </div>
+      
     </div>
   );
 };
