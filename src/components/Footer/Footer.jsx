6fb1ed9fd8c553ac8b30b18d6985452167a4a4ec
Idris Yusuf <yusid03@gmail.com>
2023-09-14 06:58:29 +0100

    Footer Build

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import {
+  BiLogoFacebookSquare,
+  BiLogoInstagram,
+  BiLogoTwitter,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.mainFooter}>
+      <div className={styles.footerCol1}>
+        <ul>
+          <li>{<BiLogoFacebookSquare />}</li>
+          <li>{<BiLogoInstagram />}</li>
+          <li>{<BiLogoTwitter />}</li>
+          <li>{<BiLogoYoutube />}</li>
+        </ul>
+      </div>
+      <div className={styles.footerCol2}>
+        <ul>
+          <li>Conditions of Use</li>
+          <li>Privacy & Policy</li>
+          <li>Press Room</li>
+        </ul>
+      </div>
+      <div className={styles.footerCol3}>
+        <span>© 2021 MovieBox by Adriana Eka Prayudha</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
